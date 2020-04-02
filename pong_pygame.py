9d94cd661d49d1c124ea1e96eea29703a7b48735
Ziad <ziad1991@hotmail.co.uk>
2020-04-02 12:30:44 +0100

    add animation to window
    draw ball and paddles into window
    add boundaries for bounce
    animation for ball and paddle

--- pong_pygame.py
+++ pong_pygame.py
@@ -1,5 +1,25 @@
 import pygame, sys
 
+def ball_animation():
+  global ball_speed_x, ball_speed_y #try to change global later
+  ball.x += ball_speed_x
+  ball.y += ball_speed_y
+
+  if ball.top <= 0 or ball.bottom >= screen_height:
+    ball_speed_y *= -1
+  if ball.left <= 0 or ball.right >= screen_width:
+    ball_speed_x *= -1
+  
+  if ball.colliderect(player) or ball.colliderect(opponent):
+    ball_speed_x *= -1
+
+def player_animation():
+  player.y += player_speed
+  if player.top <= 0:
+    player.top = 0
+  if player.bottom >= screen_height:
+    player.bottom = screen_height
+
 # General setup
 pygame.init()
 clock = pygame.time.Clock()
@@ -15,12 +35,43 @@ ball = pygame.Rect(screen_width/2 - 15, screen_height/2 - 15, 30, 30)
 player = pygame.Rect(screen_width - 20, screen_height/2 -70, 10, 140)
 opponent = pygame.Rect(10, screen_height/2 - 70, 10, 140)
 
+bg_color = pygame.Color('grey12')
+very_orange = (255,165,0)
+
+ball_speed_x = 7
+ball_speed_y = 7
+player_speed = 0
+
 while True:
   # Handling input
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       pygame.quit()
       sys.exit()
+
+    if event.type == pygame.KEYDOWN:
+      if event.key == pygame.K_DOWN:
+        player_speed += 7
+      if event.key == pygame.K_UP:
+        player_speed -= 7
+
+    if event.type == pygame.KEYUP:
+      if event.key == pygame.K_DOWN:
+        player_speed -= 7
+      if event.key == pygame.K_UP:
+        player_speed += 7
+        
+
+  ball_animation()
+  player_animation()
+
+  # Visuals
+  screen.fill(bg_color)
+  pygame.draw.rect(screen,very_orange, player)
+  pygame.draw.rect(screen,very_orange, opponent)
+  pygame.draw.ellipse(screen,very_orange, ball)
+  pygame.draw.aaline(screen, very_orange, (screen_width/2, 0), (screen_width/2,screen_height))
+
 
   # Updating the window
   pygame.display.flip()
