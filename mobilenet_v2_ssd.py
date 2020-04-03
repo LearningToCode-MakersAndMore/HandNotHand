d6bede6a05ed53839ebdbd775b1736f2cd9d38e7
Peter <pc.dean@icloud.com>
2020-04-03 11:03:46 +0100

    modified learning

--- mobilenet_v2_ssd.py
+++ mobilenet_v2_ssd.py
@@ -1,4 +1,5 @@
-from keras.preprocessing.image import ImageDataGenerator
+from keras.preprocessing.image import ImageDataGenerator, load_img
+from matplotlib import pyplot as plt
 import numpy as np
 from keras.applications import mobilenet_v2
 from keras import models
@@ -10,16 +11,17 @@ mobnet_conv = mobilenet_v2.MobileNetV2(input_shape=None, alpha=1.0, include_top=
 train_dir = './clean-dataset/train'
 validation_dir = './clean-dataset/validation'
 
-nTrain = 3840
-# nTrain = 6
+# nTrain = 3840
+nTrain = 20
 nVal = 960
-nImages = 4800
+# nVal = 200
+# nImages = 4800
 
 datagen = ImageDataGenerator(rescale=1./255)
-batch_size = 2
+batch_size = 20
 
 train_features = np.zeros(shape=(nTrain, 7, 7, 1280))
-train_labels = np.zeros(shape=(nTrain,1))
+train_labels = np.zeros(shape=(nTrain,2))
 train_generator = datagen.flow_from_directory(
     train_dir,
     target_size=(224, 224),
@@ -32,7 +34,6 @@ for inputs_batch, labels_batch in train_generator:
     train_features[i * batch_size : (i + 1) * batch_size] = features_batch
     train_labels[i * batch_size : (i + 1) * batch_size] = labels_batch
     i += 1
-    print(i)
     if i * batch_size >= nTrain:
         break
 
@@ -41,4 +42,70 @@ train_features = np.reshape(train_features, (nTrain, 7 * 7 * 1280))
 model = models.Sequential()
 model.add(layers.Dense(256, activation='relu', input_dim=7 * 7 * 1280))
 model.add(layers.Dropout(0.5))
-model.add(layers.Dense(1, activation='softmax'))
+model.add(layers.Dense(2, activation='softmax'))
+
+model.compile(optimizer=optimizers.RMSprop(lr=2e-4),
+              loss='categorical_crossentropy',
+              metrics=['acc'])
+
+validation_features = np.zeros(shape=(nVal, 7, 7, 1280))
+validation_labels = np.zeros(shape=(nVal,2))
+validation_generator = datagen.flow_from_directory(
+    validation_dir,
+    target_size=(224, 224),
+    batch_size=batch_size,
+    class_mode='categorical')
+
+i = 0
+for inputs_batch, labels_batch in validation_generator:
+    features_batch = mobnet_conv.predict(inputs_batch)
+    validation_features[i * batch_size : (i + 1) * batch_size] = features_batch
+    validation_labels[i * batch_size : (i + 1) * batch_size] = labels_batch
+    i += 1
+    if i * batch_size >= nVal:
+        break
+
+validation_features = np.reshape(validation_features, (nVal, 7 * 7 * 1280))
+
+# print(train_labels)
+# print(validation_labels)
+
+history = model.fit(train_features,
+                    train_labels,
+                    epochs=1,
+                    batch_size=batch_size,
+                    validation_data=
+(validation_features,validation_labels))
+
+fnames = validation_generator.filenames
+
+ground_truth = validation_generator.classes
+
+label2index = validation_generator.class_indices
+
+# Getting the mapping from class index to class label
+idx2label = dict((v,k) for k,v in label2index.items())
+
+predictions = model.predict_classes(validation_features)
+prob = model.predict(validation_features)
+
+errors = np.where(predictions != ground_truth)[0]
+print("No of errors = {}/{}".format(len(errors),nVal))
+# print(len(errors))
+for i in range(len(errors)):
+    # print(i)
+    pred_class = np.argmax(prob[errors[i]])
+    # print(idx2label)
+    # print(pred_class)
+    pred_label = idx2label[0]
+    # correc pred label
+    # pred_label = idx2label[pred_class]
+
+    print('Original label:{}, Prediction :{}, confidence : {:.3f}'.format(
+        fnames[errors[i]].split('/')[0],
+        pred_label,
+        prob[errors[i]][pred_class]))
+
+    # original = load_img('{}/{}'.format(validation_dir,fnames[errors[i]]))
+    # plt.imshow(original)
+    # plt.show()
